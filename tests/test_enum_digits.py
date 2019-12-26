9392ad80050b1373031fe268949255adbf935bb9
MaxBondABE <max.o.bond@gmail.com>
2019-12-25 21:55:29 -0700

    Added a more comprehensive testcase to test_enum_digts.py.

--- tests/test_enum_digits.py
+++ tests/test_enum_digits.py
@@ -7,6 +7,26 @@ examples = {
     2: ((2,), ),
     4: ((2, 2), (4, )),
     26: ((2, 2, 3), (4, 3), (2, 6)),
+    22233357: (
+        (2,2,2,3,3,3,5,7), # Do nothing,
+
+        (2,4,3,3,3,5,7), # Combining 2s
+        (8,3,3,3,5,7),
+
+        (2,2,3,3,5,6,7), # Combinging 2s and 3s
+        (2,3,5,6,6,7),
+        (5,6,6,6,7), 
+
+        (2,2,2,3,5,7,9), # Combining 3s
+
+        (2,2,5,6,7,9), # Making 9 & combining 2 & 3
+
+        (2,3,4,5,7,9), # Making 9 & combining 2s
+        (3,5,7,8,9),
+
+        (4,5,6,7,9), # Making 9, 6, and 4
+
+    )
 }
 @pytest.mark.parametrize("testcase, expected", examples.items())
 def test_enum_digits_properly_processes_input(testcase, expected):
