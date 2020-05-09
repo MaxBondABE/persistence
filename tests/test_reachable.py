836e7717ba60708ddc769bfcff7246dc24e87662
MaxBondABE <max.o.bond@gmail.com>
2020-05-09 01:57:13 -0600

    Updated test_reachable.py to reflect reachable()'s update specification.

--- tests/test_reachable.py
+++ tests/test_reachable.py
@@ -13,13 +13,13 @@ unreachableExamples =(
 )
 @pytest.mark.parametrize("testcase", unreachableExamples)
 def test_reachable_correctly_identifies_unreachable_numbers(testcase):
-    assert not reachable(testcase)
+    assert reachable(testcase) == (False, None)
 
-reachableExamples =(
-    10,
-    100,
-    (2**10 * 3**10 * 5**10 * 7**10)
-)
-@pytest.mark.parametrize("testcase", reachableExamples)
-def test_reachable_correctly_identifies_reachable_numbers(testcase):
-    assert reachable(testcase)
+reachableExamples = {
+    10: (2, 5),
+    100: (2, 2, 5, 5),
+    (2 * 3 * 5 * 7): (2, 3, 5, 7)
+}
+@pytest.mark.parametrize("testcase, expected", reachableExamples.items())
+def test_reachable_correctly_identifies_reachable_numbers(testcase, expected):
+    assert reachable(testcase) == (True, expected)
