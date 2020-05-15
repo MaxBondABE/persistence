6ec9a327cd4da5a88dc6b0cc65bee5d3da71d225
MaxBondABE <max.o.bond@gmail.com>
2020-05-14 21:23:50 -0600

    Added property testing to test.test_composit_combinations.py.
    Tested the invariant that the testcase itself is inside the output of
    persistence.core.compositeCombinations(). If there is some possibility
    that is neglected, then there should be some number for which this
    invariant doesn't hold, and visa versa - if there is no number for
    which this invariant doesn't hold, there are no possibilities that
    are neglected.
    
    I had to create a new Hypothesis strategy because of the behavior of
    persistence.core.normalize(). 0s would cause ValueErrors, and 1s would
    be stripped out, creating false positives.
    
    I also removed the comments explaining why the type testing was removed,
    fixed a typo in anohther comment, and made a note that the remaining
    type checking should be removed. This sort of thing should be implemented
    using mypy type annotations.

--- tests/strategies.py
+++ tests/strategies.py
@@ -3,3 +3,9 @@ from hypothesis import strategies as st
 digits = st.integers(min_value=0, max_value=9)
 digit_tuples = st.builds(tuple, st.lists(digits, min_size=1))
 # st.tuples does not mutate length, so must use st.lists
+
+normalized_digit_tuples = st.builds(
+    tuple,
+    st.lists(st.integers(min_value=2, max_value=9), min_size=1)
+)
+
--- tests/test_composite_combinations.py
+++ tests/test_composite_combinations.py
@@ -1,8 +1,8 @@
 import pytest
-from tests.constants import BAD_TYPES
+from tests.strategies import digit_tuples, normalized_digit_tuples
+from hypothesis import given
 
 from persistence.core import compositeCombinations
-
 
 examples = {
     2: ((2,), ),
@@ -14,7 +14,7 @@ examples = {
         (2,3,3,3,4,5,7), # Combining 2s
         (3,3,3,5,7,8),
 
-        (2,2,3,3,5,6,7), # Combinging 2s and 3s
+        (2,2,3,3,5,6,7), # Combining 2s and 3s
         (2,3,5,6,6,7),
         (5,6,6,6,7), 
 
@@ -43,5 +43,18 @@ def test_composite_combinations_properly_processes_input(testcase, expected):
     assert len(actual) == len(expected)
     # Catches repeated entries
 
-# TypeErrors and ValueErrors should be caught by normalize() and digits(), so
-# until that assumption proves faulty, there's no need to test them here.
+@pytest.mark.parametrize("testcase", examples.keys())
+def test_composite_combinations_returns_correct_type(testcase):
+    # FIXME this is really a job for mypy.
+    assert not False in map(
+        lambda a: isinstance(a, tuple) and not False in map(
+            lambda b: isinstance(b, int),
+            a
+        ),
+        compositeCombinations(testcase)
+    )
+
+@given(normalized_digit_tuples)
+def test_composite_combinations_of_number_includes_itself(testcase):
+    assert tuple(sorted(testcase)) in \
+        map(lambda a: tuple(sorted(a)), compositeCombinations(testcase))
